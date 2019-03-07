d55d04b6c4cba240447973a42de119fee4249371
krzysztofhejna <khejna15@wp.pl>
2019-03-07 17:31:19 +0100

    Create necessary components

--- src/Comment.js
+++ src/Comment.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Comment = ({text, votes}) => <li>{text} <span>votes: {votes}</span></li>;
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) => (
+  <li>
+    {text} <span>votes: {votes}</span>
+    <button onClick={() => thumbUpComment(id)}>+1</button>
+    <button onClick={() => thumbDownComment(id)}>-1</button>
+  </li>
+);
 
 export default Comment;
